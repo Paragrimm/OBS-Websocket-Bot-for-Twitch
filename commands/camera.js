01970c5131af2069c9414cf42e4a5379052740f1
Paragrimm <paragrimm@protonmail.com>
2021-01-24 03:01:48 +0100

    feat: update camerapos.json if command is executed

--- commands/camera.js
+++ commands/camera.js
@@ -1,47 +1,44 @@
 const { obs } = require("../bot.js");
-const camerapositions = require("../json/camerapos.json");
 
 exports.run = (client, message, args, context, channel, self) => {
-    if (args.length === 0) return;
-    if (context.mod || context.badges["broadcaster"] === "1") {
-        if (camerapositions[args] !== undefined) {
-            obs.sendCallback(
-                "SetSceneItemProperties",
-                {
-                    item: process.env.OBS_ITEMCAMERA,
-                    position: {
-                        x: camerapositions[args].x,
-                        y: camerapositions[args].y,
-                    },
-                },
-                (err, data) => {
-                    if (
-                        err &&
-                        err.status === "error" &&
-                        err.error === "specified scene item doesn't exist"
-                    ) {
-                        client.say(
-                            process.env.KANALNAME,
-                            "Diese Quelle existiert nicht."
-                        );
-                        return false;
-                    }
-                    if (data && data.status === "ok") {
-                        return client.say(
-                            process.env.KANALNAME,
-                            `Neue Kameraposition: ${args}`
-                        );
-                    }
-                }
-            );
-        } else {
-            client.say(
-                process.env.KANALNAME,
-                "Diese Position wurde nicht festgelegt."
-            );
-            return false;
-        }
+	const camerapositions = require("../json/camerapos.json");
+	if (args.length === 0) return;
+	if (context.mod || context.badges["broadcaster"] === "1") {
+		if (camerapositions[args] !== undefined) {
+			obs.sendCallback(
+				"SetSceneItemProperties",
+				{
+					item: process.env.OBS_ITEMCAMERA,
+					position: {
+						x: camerapositions[args].x,
+						y: camerapositions[args].y,
+					},
+				},
+				(err, data) => {
+					if (
+						err &&
+						err.status === "error" &&
+						err.error === "specified scene item doesn't exist"
+					) {
+						client.say(process.env.KANALNAME, "Diese Quelle existiert nicht.");
+						return false;
+					}
+					if (data && data.status === "ok") {
+						return client.say(
+							process.env.KANALNAME,
+							`Neue Kameraposition: ${args}`
+						);
+					}
+				}
+			);
+		} else {
+			client.say(
+				process.env.KANALNAME,
+				"Diese Position wurde nicht festgelegt."
+			);
+			return false;
+		}
 
-        return true;
-    }
+		return true;
+	}
 };
